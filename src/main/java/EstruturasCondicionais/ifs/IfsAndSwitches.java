00c0d838e59126beb37ce53145ea288ec5ce8840
Wanderson Frauches <wfrauches89@gmail.com>
2024-06-07 09:07:08 -0300

    Estruturas condicionais If/Else, While and Switch

---
+++ src/main/java/EstruturasCondicionais/ifs/IfsAndSwitches.java
@@ -0,0 +1,47 @@
+package EstruturasCondicionais.ifs;
+
+import java.util.Scanner;
+
+public class IfsAndSwitches {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String grade;
+
+            //if-else
+            System.out.println("Qual a nota do aluno?");
+            int notaRecebida = scanner.nextInt();
+
+                if (notaRecebida >= 0 && notaRecebida < 60) {
+                    grade = "D";
+                } else if (notaRecebida >= 60 && notaRecebida < 70) {
+                    grade = "C";
+                } else if (notaRecebida >= 70 && notaRecebida < 80) {
+                    grade = "B";
+                } else if (notaRecebida >=80){
+                    grade = "A";
+                } else {
+                    grade = "0";
+                }
+
+        switch (grade){
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado");
+                break;
+            case "C":
+                    System.out.println("Aluno em recuperação");
+                    break;
+            case "D":
+                    System.out.println("Aluno reprovado");
+                    break;
+            default: System.out.println("Entre com uma nota válida");
+        }
+
+
+        System.out.println("                             ");
+        System.out.println("Fim do programa!");
+
+    }
+}
---
+++ src/main/java/EstruturasCondicionais/ifs/IfsAndWhiles.java
@@ -0,0 +1,43 @@
+package EstruturasCondicionais.ifs;
+
+import java.util.Scanner;
+
+public class IfsAndWhiles {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int exit = 1;
+
+        while(exit != 0) {
+            //if-else
+            System.out.println("Qual a nota do aluno?");
+            int notaRecebida = scanner.nextInt();
+            if(notaRecebida <0 && notaRecebida >100) {
+                System.out.println("Nota inválida");
+            } else {
+                if (notaRecebida >= 0 && notaRecebida < 60) {
+                    System.out.println("                             ");
+                    System.out.println("Aluno reprovado");
+                    System.out.println("                             ");
+                } else if (notaRecebida >= 60 && notaRecebida < 70) {
+                    System.out.println("                             ");
+                    System.out.println("Aluno recuperação");
+                    System.out.println("                             ");
+                } else {
+                    System.out.println("                             ");
+                    System.out.println("Aluno aprovado");
+                    System.out.println("                             ");
+                }
+            }
+            System.out.println(" -----> Se deseja sair pressione 0 <-----");
+            System.out.println("                             ");
+            System.out.println(" -----> Se deseja continuar tecle qualquer número <-----");
+            exit = scanner.nextInt();
+        }
+
+        System.out.println("                             ");
+        System.out.println("Fim do programa!");
+
+    }
+}
