9cc6c53c80d6b69e972e4fcacc98c28f19ba0af5
Wanderson Frauches <wfrauches89@gmail.com>
2024-06-07 10:02:24 -0300

    trabalhando com for

---
+++ src/main/java/Repeticoes/ForRep.java
@@ -0,0 +1,20 @@
+package Repeticoes;
+
+public class ForRep {
+    public static void main(String[] args) {
+        for(int i = 1; i <=5; i++) {
+            System.out.println("For repetição: "+i);
+        }
+
+
+        for(int j = 1; j <=10; j++) {
+            System.out.println("Tabela de multiplicação de "+j);
+            for(int i = 1; i <=10; i++) {
+                System.out.println(i+"x"+j+" = "+i*j);
+            }
+        }
+
+
+
+    }
+}
