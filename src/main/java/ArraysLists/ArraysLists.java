060db672d5156e86ca5e2bc93d27835dea71e650
Wanderson Frauches <wfrauches89@gmail.com>
2024-06-07 10:27:51 -0300

    Adicionando arrays

---
+++ src/main/java/ArraysLists/ArraysLists.java
@@ -0,0 +1,39 @@
+package ArraysLists;
+
+import java.util.Arrays;
+
+public class ArraysLists {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {15, 25, 44, 13, 125};
+
+        System.out.printf("Tamanho do array: %s",numeros.length);
+        System.out.println(" ");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        int menor = numeros [0];
+        int maior = numeros[0];
+        float media = 0;
+
+        for (int i = 0; i < numeros.length; i++){
+            if(maior < numeros[i]){
+                maior = numeros[i];
+            }
+            if(menor > numeros[i]){
+                menor = numeros[i];
+            }
+
+            media += (float) numeros[i];
+
+        }
+
+        System.out.println("Menor valor: "+menor);
+        System.out.println("Maior valor: "+maior);
+        System.out.println("Média valor: "+media/ numeros.length);
+
+    }
+}
